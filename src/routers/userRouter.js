ae02e9b1dd9425185fe62e4506b35cd6d8b4e2f9
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-19 23:32:10 +0300

    create a route to get all users

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const router = new express.Router();
 const User = require('../models/userModel')
+
+// ROUTES
+
+
+// GET all users
+router.get('/users',async (req,res)=>{
+    try{
+        const users = await User.find({})
+        res.send(users)
+    }catch(e){
+        res.status(500).send()
+    }
+})
