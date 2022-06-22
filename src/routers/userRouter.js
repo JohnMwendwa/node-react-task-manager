b1e7ed21607cedd34bf676431348379b27fcfee4
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-22 19:32:04 +0300

    create UPDATE route for updating user information

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { findById } = require('../models/userModel');
 const router = new express.Router();
 const User = require('../models/userModel')
 
@@ -22,6 +23,24 @@ router.get('/users',async (req,res)=>{
         res.send(users)
     }catch(e){
         res.status(500).send(e.message)
+    }
+})
+
+// UPDATE a user
+router.patch('/users/:id',async (req,res)=>{
+    // determine which keys are being updated
+    const updates = Object.keys(req.body)
+    try{
+        const user = await User.findById(req.params.id)
+        if(!user){
+           throw new Error("User doesn't exist")
+        }
+        updates.forEach(update=>(user[update] = req.body[update]))
+        await user.save();
+        res.send(user)
+        
+    }catch(e){
+        res.status(400).send(e.message)
     }
 })
 
