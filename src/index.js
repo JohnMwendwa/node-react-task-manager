daa087dc12a11a7ba187695f709cb1c6a3eb3185
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-22 18:16:11 +0300

    import the user router and set express to use it

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@
 require('./db/mongoose')
 
 const express = require('express');
+const userRouter = require('./routers/userRouter')
 
 // Initialize app
 const app = express();
+
 // Environment port
 const port =process.env.PORT || 3000
+
+app.use(userRouter)
 
 //listen for connection
 app.listen(port,()=>console.log(`Server started on port ${port}`))
