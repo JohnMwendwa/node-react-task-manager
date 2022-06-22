b55376fc5c64c49cdfe1dd8a7dce1efba33a5b97
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-22 18:50:59 +0300

    create a route for signing-up new users

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -4,6 +4,16 @@ const User = require('../models/userModel')
 
 // ROUTES
 
+// CREATE a new user
+router.post('/users',async (req,res)=>{
+    try{
+     const user = new User(req.body)
+     await user.save();
+     res.status(201).send(user)
+    }catch(e){
+        res.status(400).send(e.message)
+    }
+})
 
 // GET all users
 router.get('/users',async (req,res)=>{
@@ -11,7 +21,7 @@ router.get('/users',async (req,res)=>{
         const users = await User.find({})
         res.send(users)
     }catch(e){
-        res.status(500).send()
+        res.status(500).send(e.message)
     }
 })
 
