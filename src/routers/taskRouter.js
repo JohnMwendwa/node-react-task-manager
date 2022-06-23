570b7f35d980cc7ccde1b860622d3d9f256bac0d
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-23 18:40:16 +0300

    create a DELETE /tasks/:id route to delete a single task

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -60,4 +60,20 @@ router.patch('/tasks/:id', async (req,res)=>{
     }
 })
 
+// DELETE a single task
+router.delete('/tasks/:id', async (req,res)=>{
+    try{
+        const task = await Task.findById(req.params.id);
+
+        if(!task){
+            return res.status(404).send("Task doesn't exist!")
+        }
+        await task.remove();
+
+        res.send('Task Deleted successfully')
+    }catch(e){
+        res.status(404).send("Task doesn't exist!")
+    }
+})
+
 module.exports= router;
