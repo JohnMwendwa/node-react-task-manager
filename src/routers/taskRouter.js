4f8478f9fc1ddce80a838e165f01e600f677fffc
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-23 18:04:02 +0300

    create POST /tasks route for creating new tasks

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -2,3 +2,20 @@ const express = require('express');
 const Task = require('../models/taskModel');
 const router = new express.Router();
 
+//ROUTES
+
+// CREATE a task
+router.post('/tasks',async (req,res)=>{
+    try{
+      const task = new Task(req.body)
+      if(!task){
+        throw new Error("You cannot create an empty task")
+      }
+      await task.save();
+      res.status(201).send(task)
+    }catch(e){
+        res.status(400).send(e.message)
+    }
+})
+
+module.exports= router;
