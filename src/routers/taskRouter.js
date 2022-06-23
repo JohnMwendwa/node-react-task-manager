151b786abe3608d7425976318fd8d108c0941f82
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-23 18:21:53 +0300

    create a GET /task/:id route to query a single task from the database

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -31,5 +31,18 @@ router.get('/tasks',async (req,res)=>{
     }
 })
 
+// GET a single task
+router.get('/tasks/:id',async (req,res)=>{
+    try{
+        const task = await Task.findById(req.params.id);
+        if(!task){
+            return res.status(404).send("Task doesn't exist")
+        }
+        res.send(task)
+    }catch(e){
+        res.status(404).send("Task doesn't exist")
+    }
+})
+
 
 module.exports= router;
