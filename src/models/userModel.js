d1dbd98988a9791c501511249b77118bebc518fd
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-24 11:40:52 +0300

    import bcrypt module

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema= new mongoose.Schema({
     name:{
@@ -23,6 +24,8 @@ const userSchema= new mongoose.Schema({
         minlength:7,
     }
 })
+
+
 
 const User = mongoose.model('User',userSchema);
 
