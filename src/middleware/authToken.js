e5b2f599a64d594abb50f59cd727070583db52b3
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-24 21:36:35 +0300

    create a function to authenticate the stored tokens before allowing a user to access a particular route

--- src/middleware/authToken.js
+++ src/middleware/authToken.js
@@ -1,2 +1,19 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel')
+
+const auth = async (req,res,next)=>{
+    try{
+        const token = req.header('Authorization').replace('Bearer ','');
+        const decoded = jwt.verify(token,'taskapp');
+        const user = await User.findOne({_id:decoded._id,'tokens.token':token})
+
+        if(!user){
+            throw new Error()
+        }
+        req.user = user;
+        next()
+    }catch(e){
+        res.status(401).send('Please authenticate!')
+      }
+}
+
