62299ab77e3ba69ea1c835a92c12bdd301510c9e
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-25 01:10:35 +0300

    add authentication to GET /tasks/:id route to only be accessible to users who created the task

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -36,9 +36,11 @@ router.get('/tasks',async (req,res)=>{
 })
 
 // GET a single task
-router.get('/tasks/:id',async (req,res)=>{
+router.get('/tasks/:id',auth,async (req,res)=>{
+    const _id = req.params.id
     try{
-        const task = await Task.findById(req.params.id);
+        const task = await Task.findOne({_id,author:req.user._id});
+
         if(!task){
             return res.status(404).send("Task doesn't exist")
         }
