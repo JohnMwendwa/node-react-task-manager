d89fef192dc6ca8814625269f7a5017b3fdf8777
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-25 10:44:33 +0300

    add middleware to delete all user tasks when a user deletes their account

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const Task = require('./taskModel');
 
 const userSchema= new mongoose.Schema({
     name:{
@@ -88,6 +89,13 @@ userSchema.pre('save', async function (next){
         user.password = await bcrypt.hash(user.password,8)
     }
     // call the next middleware after hashing is complete
+    next()
+})
+
+// DELETE all user tasks when user deletes their account
+userSchema.pre('remove', async function (next){
+    const user = this;
+    await Task.deleteMany({author:user._id})
     next()
 })
 
