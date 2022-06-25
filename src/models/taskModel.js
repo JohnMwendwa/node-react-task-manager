69923d14a3c15570d338b596a9a7e670c9e9e50c
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-26 00:08:26 +0300

    enable timestamps in task and user models to take advantage of sorting and filtering data

--- src/models/taskModel.js
+++ src/models/taskModel.js
@@ -15,6 +15,8 @@ const taskSchema = new mongoose.Schema({
         required:true,
         ref:'User'
     }
+},{
+    timestamps:true
 });
 
 const Task = mongoose.model('Task',taskSchema);
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -33,6 +33,8 @@ const userSchema= new mongoose.Schema({
             required:true
         }
     }]
+},{
+    timestamps:true
 })
 
 // Create a vitual field for storing tasks
