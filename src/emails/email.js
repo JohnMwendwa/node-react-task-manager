9661b30b29637e7e9d6333b1f7b07f2627296815
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-25 12:53:35 +0300

    create a sendCancellationEmail to send a 'regret' email when a user deletes their account

--- src/emails/email.js
+++ src/emails/email.js
@@ -3,17 +3,30 @@ dotenv.config({path:'config/.env'})
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendWelcomEmail = (email,name)=>{
+const sendWelcomeEmail = (email,name)=>{
     sgMail.send({
        to:email,
        from:'dev.johnmwendwa@gmail.com',
        subject:'Welcome to Task App',
        html:`<h2>Hey ${name}</h2>
-       <p>Welcome we're glad to see you're using Task App to plan your daily tasks.Please Feel free to recommend any improvements that you might like see.</p>
+       <p>Welcome, we're glad to see you're using Task App to plan your daily tasks.Please Feel free to recommend any improvements that you might like see.</p>
        `
     })
    }
    
+   const sendCancellationEmail = (email,name)=>{
+    sgMail.send({
+        to:email,
+        from:'dev.johnmwendwa@gmail.com',
+        subject:'We regret to see you leave',
+        html:`<h2>Hey ${name}</h2>
+        <p>We regret to see you leave. We hope to see you back soon.</p>
+        <p>Kindly suggest any feature improvement that you'd like to see in the app</p>
+        `
+    })
+   }
+
    module.exports = {
-    sendWelcomEmail
+    sendWelcomeEmail,
+    sendCancellationEmail
 }
