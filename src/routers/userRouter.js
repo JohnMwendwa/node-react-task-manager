867d04701940e06dbca55e8cf493ee2be34fc9c6
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-25 13:07:44 +0300

    import sendCancellationEmail function to send our 'regrets' after user a deletes their account

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const User = require('../models/userModel')
 const auth = require('../middleware/authToken');
-const {sendWelcomeEmail} = require('../emails/email');
+const {sendWelcomeEmail,sendCancellationEmail} = require('../emails/email');
 const router = new express.Router();
 
 // ROUTES
@@ -80,6 +80,7 @@ router.patch('/users/me',auth,async (req,res)=>{
 router.delete('/users/me',auth, async (req,res)=>{
     try{
         await req.user.remove()
+        sendCancellationEmail(req.user.email,req.user.name)
         res.send(req.user)
     }catch(e){
         res.status(404).send(e.message)
