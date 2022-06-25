8d4a04a9f71edccce845219d1814b8637b724a1d
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-25 12:28:06 +0300

    create a function to send welcome message email once a user creates their account

--- src/emails/email.js
+++ src/emails/email.js
@@ -3,3 +3,14 @@ dotenv.config({path:'config/.env'})
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+const sendWelcomEmail = (email,name)=>{
+    sgMail.send({
+       to:email,
+       from:'dev.johnmwendwa@gmail.com',
+       subject:'Welcome to Task App',
+       html:`<h2>Hey ${name}</h2>
+       <p>Welcome we're glad to see you're using Task App to plan your daily tasks.Please Feel free to recommend any improvements that you might like see.</p>
+       `
+    })
+   }
+   
