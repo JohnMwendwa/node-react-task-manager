35b316d2e5e692445127e7bebdab4fba2afdc3c7
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-27 02:02:03 +0300

    move the JWT token secret key to environment variables

--- src/middleware/authToken.js
+++ src/middleware/authToken.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel')
+const secret = process.env.CONSUMER_SECRET;
 
 const auth = async (req,res,next)=>{
     try{
         const token = req.header('Authorization').replace('Bearer ','');
-        const decoded = jwt.verify(token,'taskapp');
+        const decoded = jwt.verify(token,secret);
         const user = await User.findOne({_id:decoded._id,'tokens.token':token})
 
         if(!user){
