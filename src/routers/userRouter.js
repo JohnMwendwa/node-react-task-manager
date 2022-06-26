daadff3d67b9c79ec4540f907dca406e4caac97c
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-26 21:26:32 +0300

    only allow images of type jpg,jpeg or png

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -10,6 +10,12 @@ const upload = multer({
     // limit image files to 1MB size
     limits:{
         fileSize:1000000
+    },
+    fileFilter(req,file,cb){
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            return cb(new Error('Please upload an image of type jpg,jpeg or png'))
+        }
+        cb(null,true)
     }
 })
 
