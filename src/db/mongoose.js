37eea2454e9c6463ef05559422dd0ab63a6b0afa
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-27 01:53:31 +0300

    move mongodb url to environment variables

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,11 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({path:'config/.env'})
+// mongodb url
+const url = process.env.MONGODB_URL;
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-app')
+mongoose.connect(url)
+ .then(
+    (res)=>{console.log('connected to the database')},
+    err=>{console.log('could not connect to the database')}
+ )
