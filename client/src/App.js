d029ec29a2edfb9d2c720878ca49e62481d17837
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-28 21:52:41 +0300

    import login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
      <Signup />
+     <Login />
     </div>
   );
 }
