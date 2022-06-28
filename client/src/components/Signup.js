28b4a2d2b8955a727e5faaa0d37f6ece5b605931
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-28 21:13:38 +0300

    handle submit data and persist the token to local storage

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -4,9 +4,25 @@ function Signup() {
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
     const [email,setEmail] =useState('');
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await fetch('http://localhost:3000/users', {
+          method: 'POST',
+          body: JSON.stringify({email,password,name}),
+          headers: { 
+              'Content-Type': 'application/json',
+              'mode':'cors'
+          },
+        })
+          .then(res => res.json())
+          .then(data =>
+            localStorage.setItem('token',`Bearer ${data.token}`))
+      }
     
     return (
-      <form >
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="username"
