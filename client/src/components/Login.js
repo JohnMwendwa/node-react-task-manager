e93360ba41186e14fd91c5f3264dd4d4c2c33dc4
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-29 12:21:23 +0300

    import stylesheet

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react'
 import { useNavigate } from 'react-router-dom';
 import Navbar from './Navbar';
+import './css/Login.css'
 
 function Login() {
     const [password, setPassword] = useState('');
