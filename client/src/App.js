beb2aec7df881cd42e38047f4f2b9b3273bd1dfb
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-29 11:26:46 +0300

    add route to dashboard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import {Route, Routes} from 'react-router-dom'
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
+      <Routes>       
         <Route path='/' element={<Signup />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard />} />
         <Route path='*' element={<Login />} />
       </Routes>
     </div>
