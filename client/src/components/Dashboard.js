85c0a1275265befed32dbdca91cb0bd84e08c1fa
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-29 11:25:22 +0300

    add function to fetch user and tasks and render user tasks

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react"
+
+function Dashboard() {
+    const [tasks,setTasks] = useState([]);
+    const [username,setUsername] = useState('');
+    const [email,setEmail] = useState('');
+    const token = localStorage.getItem('token');
+
+    useEffect(()=>{
+        const getUser = async ()=>{
+            await fetch('http://localhost:3000/users/me',{
+               method:'GET',
+               headers:{
+               'Authorization':token
+           }
+           }).then(res=>res.json())
+             .then(data=> {
+                setUsername(data.name);
+                setEmail(data.email)
+            })
+       }
+   
+       const getTasks = async ()=>{
+            await fetch('http://localhost:3000/tasks',{
+               method:'GET',
+               headers:{
+               'Authorization':token
+           }
+           }).then(res=>res.json())
+             .then(data=>setTasks(data))
+   
+       }
+
+        getUser();
+        getTasks()
+    },[token])
+
+  return (
+    <div>
+    <nav>
+        <div> {username}</div>
+        <div> {email}</div>
+    </nav>
+    <h1>Tasks</h1>
+    {tasks.map(task=><li key={task._id}>{task.description}</li>)}
+    </div>
+  )
+}
+
+export default Dashboard
