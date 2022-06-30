c1bf3726088aee9b7d24187ce0499d84431797b7
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-06-30 07:10:26 +0300

    set the app index page

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const port =process.env.PORT
 
 // set path to react build folder
 const reactAssets = path.join(__dirname,'../client/build');
+const indexPage = path.join(__dirname,'../client/build','index.html');
 
 //set express server to serve react static assets
 app.use(express.static(reactAssets));
@@ -21,6 +22,10 @@ app.use(express.static(reactAssets));
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+
+app.get('/*',(req,res)=>{
+    res.sendFile(indexPage)
+})
 
 //listen for connection
 app.listen(port,()=>console.log(`Server started on port ${port}`))
