6255a2a42bb8e84a6fb5b83231e924245d38ba12
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-01 09:25:01 +0300

    add contact information

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,13 +10,13 @@ function Footer() {
         </div>
     <div className="Footer__copyright">
         <div className="Footer__copyright-text">
-            <p>Copyright &copy; 2022, All Rights Reserved <a href="#!">John M</a></p>
+            <p> &copy; 2022 <a href="dev.johnmwendwa@gmail.com">John Mwendwa</a></p>
         </div>
         <div className="Footer__menu">
             <ul>
                 <li><a href="#!">Terms</a></li>
                 <li><a href="#!">Privacy</a></li>
-                <li><a href="#!">Contact</a></li>
+                <li><a href="dev.johnmwendwa@gmail.com">Contact</a></li>
             </ul>
         </div>
     </div>
