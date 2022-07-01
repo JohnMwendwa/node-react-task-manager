d0ec134614d1b5fa19020c5a8c9e164c048316d9
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-01 08:24:47 +0300

    add footer content

---
+++ client/src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Footer() {
+  return (
+   <footer className='Footer'>
+    <div className="Footer__container">
+        <div className="Footer__information">
+            
+        </div>
+    <div className="Footer__copyright">
+        <div className="Footer__copyright-text">
+            <p>Copyright &copy; 2022, All Rights Reserved <a href="#!">John M</a></p>
+        </div>
+        <div className="Footer__menu">
+            <ul>
+                <li><a href="#!">Terms</a></li>
+                <li><a href="#!">Privacy</a></li>
+                <li><a href="#!">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+    </div>
+   </footer>
+  )
+}
+
+export default Footer
