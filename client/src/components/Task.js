6793ab8385406f397cd95b2d5bc291f0d31eb6a4
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-03 22:02:29 +0300

    toggle EditTaskForm and task

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import EditTaskForm from './EditTaskForm'
+import useToggle from './hooks/useToggle'
 
 function Task({task}) {
+    const [isEdit,setIsEdit] = useToggle(false);
   return (
-    <div>
+
+        isEdit ? <EditTaskForm  task={task} edit={setIsEdit}/>:<div>
         <li>{task.description}</li>
-        <button>Edit</button>
+        <button onClick={setIsEdit}>Edit</button>
         <button>Delete</button>
-    </div>
+        </div>
+        
   )
 }
 
