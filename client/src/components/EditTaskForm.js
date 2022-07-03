4bded0f81923d9065ab9f8eb9c36616e8c34ffe9
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-03 21:57:25 +0300

    create an EditTaskForm to edit and update individual tasks

--- client/src/components/EditTaskForm.js
+++ client/src/components/EditTaskForm.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import useFormInput from './hooks/useFormInput';
 
-function EditTaskForm({task}) {
+function EditTaskForm({task,edit}) {
     const [oldTask,updateTask,resetTask] = useFormInput(task.description);
+const token = localStorage.getItem('token')
+
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        try{
+            const updateTask =async()=>{
+                await fetch(`/tasks/${task._id}`,{
+                    method:'PATCH',
+                    body:JSON.stringify({description:oldTask}),
+                    headers:{
+                    'Content-Type':'application/json',
+                    'Authorization':token  
+                 }
+                });
+            }
+            updateTask()
+        }catch(e){
+            console.log(e)
+        }
+        resetTask();
+        edit(); 
+    }
   return (
-   <form >
+   <form onSubmit={handleSubmit}>
     <input type="text"  value={oldTask} onChange={updateTask}/>
-    <button onClick={resetTask}>Save</button>
+    <button >Save</button>
    </form>
   )
 }
