95e14d23ed90145544a546fe6681874b4286af02
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-12 00:59:12 +0300

    prevent making a fetch request if a user doesn't have a bearer token

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,4 +1,5 @@
 import React,{useContext, useEffect, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 import { getTasks } from '../services/service';
 import Task from './Task'
 import './css/TaskList.css'
@@ -7,8 +8,12 @@ import { TaskContext } from '../contexts/TaskContext';
 function TaskList() {
   const {token,mounted,updated,setUpdated} = useContext(TaskContext)
   const [tasks,setTasks] = useState([]);
+  let navigate = useNavigate();
 
   useEffect(()=>{
+    if(!token){
+      return navigate('/login')
+    }
     getTasks(token)
      .then(data=>{
       if(mounted.current || updated){
@@ -16,7 +21,7 @@ function TaskList() {
         setUpdated(false)
       }
      })
-  },[mounted,token,updated,setUpdated])
+  },[mounted,token,updated,setUpdated,navigate])
   return (
     <div className='TaskList'>
      <ul className="TaskList__lists">
