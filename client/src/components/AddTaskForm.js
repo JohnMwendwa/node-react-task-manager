d8a862a719a205406c6bcff8611aad60a8e851dc
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-11 10:39:39 +0300

    create tasks from the AddTaskForm component and post them to the database

--- client/src/components/AddTaskForm.js
+++ client/src/components/AddTaskForm.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import useFormInput from '../hooks/useFormInput';
+import { createTask } from '../services/service';
 import './css/AddTaskForm.css';
 
-function AddTaskForm({createTask}) {
-const [task,handleTask,resetTask] = useFormInput('');
+function AddTaskForm() {
+const [task,setTask,resetTask] = useFormInput('');
+const token = localStorage.getItem('token');
 
 const handleSubmit =(e)=>{
     e.preventDefault();
-    try{
-         createTask(task)     
-    }catch(e){
-        console.log(e)
-    }
+    
+    if(token){
+      createTask(task,token)  
+    }   
     resetTask()
 }
 
   return ( 
     <form onSubmit={handleSubmit} className='AddTaskForm'>
-       <input type="text" value={task} onChange={handleTask}  className='AddTaskForm__input' />
+       <input type="text" value={task} onChange={setTask}  className='AddTaskForm__input' />
        <button className='AddTaskForm__btn'>Add Task</button>
     </form>
   )
