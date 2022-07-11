af950aea9159db29171da74971741f02e6647af4
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-11 11:13:59 +0300

    logout user and navigate them to the login page

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,8 +1,24 @@
+import {logoutUser} from '../services/service';
+import {useNavigate} from 'react-router-dom';
 
-function Logout({logoutUser}) {
-     
+function Logout() {
+  let token = localStorage.getItem('token');
+  let navigate = useNavigate();
+
+const handleLogout =()=>{
+    if(token){
+      logoutUser(token)
+      .then(()=>{
+        token = localStorage.setItem('token','');
+        navigate('/login')
+      })
+    }
+}
+  
   return (
-    <button onClick={()=>logoutUser()}>Logout</button>
+    <button onClick={handleLogout}>
+      Logout
+    </button>
   )
   }
 
