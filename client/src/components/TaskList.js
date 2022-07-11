d40aa9e124eff0ec6ec87153089f032d5e687ee0
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-11 10:16:49 +0300

    fetch all tasks from the database

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react';
+import { getTasks } from '../services/service';
 import Task from './Task'
 import './css/TaskList.css'
 
-function TaskList({tasks,updateTask}) {
+function TaskList() {
+  const [tasks,setTasks] = useState([]);
+  const token = localStorage.getItem('token');
+
+  useEffect(()=>{
+    getTasks(token)
+     .then(data=>{
+      setTasks(data)
+     })
+  },[token])
   return (
     <div className='TaskList'>
      <ul className="TaskList__lists">
         {tasks.map(task=>
-              <Task key={task._id} task={task} updateTask={updateTask}/>)}
+              <Task key={task._id} task={task} />)}
           </ul>
     </div>
   )
