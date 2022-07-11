99795691760559efd064035e8702dccdd1eaf987
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-11 11:34:19 +0300

    delete a task when you click the delete button

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import EditTaskForm from './EditTaskForm'
-import useToggle from '../hooks/useToggle'
+import useToggle from '../hooks/useToggle';
+import { deleteTask } from '../services/service';
 import './css/Task.css'
 
 
 function Task({task}) {
     const [isEdit,setIsEdit] = useToggle(false);
+    const token = localStorage.getItem('token');
+
+    const handleDelete =()=>{
+      deleteTask(task._id,token)
+    }
+
   return (
 
         isEdit ? <EditTaskForm  task={task} edit={setIsEdit} />:
@@ -14,7 +21,7 @@ function Task({task}) {
 
           <div className="Task__btns">
             <button onClick={setIsEdit} className='Task__btn'>Edit</button>
-            <button className='Task__btn'>Delete</button>
+            <button onClick={handleDelete} className='Task__btn'>Delete</button>
           </div>
         </div>
         
