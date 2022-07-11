39f00c57b5d25f7c7561459296ed6155a10cc13b
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-11 19:04:48 +0300

    wrap the Dashboard component with the TaskContextProvider

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,20 @@ import Signup from "./components/Signup";
 import Login from "./components/Login";
 import {Route, Routes} from 'react-router-dom'
 import Dashboard from "./components/Dashboard";
+import { TaskContextProvider } from "./contexts/TaskContext";
 
 function App() {
   return (
     <div className="App">
+      
       <Routes>       
         <Route path='/' element={<Signup />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/dashboard' element={
+          <TaskContextProvider>
+                <Dashboard />
+          </TaskContextProvider>
+        } />
         <Route path='*' element={<Login />} />
       </Routes>
     </div>
