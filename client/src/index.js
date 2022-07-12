85e3e70e814b2cb4648e97ed443b8cdac9c3bfa2
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-07-12 12:25:58 +0300

    wrap the whole app component with the TaskProviderContext

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
+import { TaskContextProvider } from "./contexts/TaskContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-       <App />
+      <TaskContextProvider>
+           <App />
+      </TaskContextProvider>
     </React.StrictMode>
   </BrowserRouter>
 );
